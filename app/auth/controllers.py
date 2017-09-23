46b7580a909d11bc44ae10183c5205e482ea5bf4
Clayton A. Alves <clayton.aa@gmail.com>
2017-09-22 22:06:43 -0400

    Fixed signin form category selection (fixes #5)

--- app/auth/controllers.py
+++ app/auth/controllers.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, flash, redirect, render_template, url_for
 
 from core.database import db
+from core.cidades.models import Cidade
+from core.categorias.models import Categoria
 from core.anunciantes.models import Anunciante
 
 from .forms import CadastroForm
@@ -11,6 +13,8 @@ auth_blueprint = Blueprint('auth', __name__, url_prefix='/cadastro')
 @auth_blueprint.route('/', methods=['GET', 'POST'])
 def auth_cadastro():
     form = CadastroForm()
+    form.cidade.choices = [(cidade.id_cidade, cidade.nome) for cidade in Cidade.query.all()]
+    form.categoria.choices = [(categoria.id_categoria, categoria.descricao) for categoria in Categoria.query.all()]
     if form.validate_on_submit():
         salva_cadastro_anunciante(form)
         flash('Cadastro efetuado!')
@@ -22,13 +26,13 @@ def salva_cadastro_anunciante(form):
     anunciante = Anunciante()
     anunciante.nome_fantasia = form.nome_fantasia.data
     anunciante.razao_social = form.razao_social.data
-    anunciante.id_cidade = int(form.cidade.data.id_cidade)
-    anunciante.id_categoria = int(form.categoria.data)
+    anunciante.id_cidade = form.cidade.data
+    anunciante.id_categoria = form.categoria.data
     anunciante.telefone = form.telefone.data
     anunciante.celular = form.celular.data
     anunciante.logradouro = form.logradouro.data
     anunciante.numero = form.numero.data
-    anunciante.bairro = form.numero.data
+    anunciante.bairro = form.bairro.data
     anunciante.email = form.email.data
     anunciante.senha = form.senha.data
     db.session.add(anunciante)
--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,10 +8,6 @@ from core.database import db
 from core.cidades.models import Cidade
 
 
-def cidades_choices():
-    return db.session.query(Cidade).all()
-
-
 class CadastroForm(FlaskForm):
     nome_fantasia = TextField(u"Nome Fantasia", validators=[DataRequired(message=u"Este campo é obrigatório")])
     razao_social = TextField(u"Razão Social")
@@ -22,5 +18,5 @@ class CadastroForm(FlaskForm):
     bairro = TextField(u"Bairro")
     email = TextField(u"E-mail")
     senha = PasswordField(u"Senha")
-    cidade = QuerySelectField(u'Cidade', query_factory=cidades_choices)
-    categoria = SelectField(u'Categoria', choices=[('1', u'Oficina'), ('2', u'Imobiliárias')])
+    cidade = SelectField(u'Cidade', validators=[DataRequired(message="Informe uma cidade")], coerce=int)
+    categoria = SelectField(u'Categoria', validators=[DataRequired(message="Informe uma categoria")], coerce=int)
