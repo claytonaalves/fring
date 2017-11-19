07a01f4a0a884c98daa9a8b08a5bd3e5ac046c18
Clayton A. Alves <clayton.aa@gmail.com>
2017-11-18 21:32:27 -0300

    Adds function to wait for mysql container startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function wait_mysql_startup {
+    echo "Waiting for mysql"
+    until mysql -h $DATABASE_HOST -u $DATABASE_USER -p$DATABASE_PASS &> /dev/null
+    do
+      printf "."
+      sleep 1
+    done
+}
+
 if [ ! -f /tmp/system_installed ]; then
     make decrypt_conf
     chmod 644 /var/www/fring/core/settings.py
@@ -10,6 +19,8 @@ if [ ! -f /tmp/system_installed ]; then
 
     npm install
     npm run postinstall
+
+    wait_mysql_startup
 
     python fixtures.py
 
